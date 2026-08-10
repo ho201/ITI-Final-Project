87f6009d343cdbbee7f74ec0241573741375c79f
Hoda Hatem <hodahatem324@gmail.com>
2026-08-10 13:21:57 +0300

    add config, jwt files

---
+++ src/config/config.js
@@ -0,0 +1,18 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const config = {
+    port: process.env.PORT || 3000,
+
+    database: {
+        uri: process.env.MONGO_URI,
+    },
+
+    jwt: {
+        secret: process.env.JWT_SECRET,
+        expiresIn: process.env.JWT_EXPIRES_IN || "7d",
+    },
+};
+
+module.exports = config;
